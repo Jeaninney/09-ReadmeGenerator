dfd374fdd832a882be5b8da1f44329860697409f
Jeaninne <jeaninnel@hotmail.com>
2020-04-09 22:07:22 -0400

    added in template of info that needs to be added.

--- index.js
+++ index.js
@@ -25,9 +25,34 @@ inquirer.prompt([
   ])
   .then(response => {
     readMe = `
+    ${response.title}  
+
+    Description: ${response.description}
+
+    Table of Contents:
+
+    Installation: What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+
+    Usage: Provide instructions and examples for use. Include screenshots as needed.
+
+    Credits: List your collaborators, if any, with links to their GitHub profiles.
+    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+    If you followed tutorials, include links to those here as well.
+
+    License: The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use https://choosealicense.com/
+
+    Badges: Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by shields.io. You may not understand what they all represent now, but you will in time.
+
+    Contributing: If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The Contributor Covenant is an industry standard, but you can always write your own.
+
+    Tests: Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+    GitHub profile picture:
+
+    GitHub email address:
+
     Username: ${response.username}
-    Project Title: ${response.title}
-    Project Description: ${response.description}
     `
     fs.writeFile("README.md", readMe, function(err) {
       
